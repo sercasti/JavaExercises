f7977d4926613ee82dfd4b443ebde380732df03b
sercasti <sercasti@gmail.com>
2017-06-05 12:17:38 -0300

    added more exercises

--- src/java/IsBST.java
+++ src/java/IsBST.java
@@ -1,5 +1,3 @@
-import java.util.LinkedList;
-
 public class IsBST {
 
 	public static void main(String args[]) {
---
+++ src/java/ShortURL.java
@@ -0,0 +1,30 @@
+
+public class ShortURL {
+
+	public static final String ALPHABET = "23456789bcdfghjkmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ-_";
+	public static final int BASE = ALPHABET.length();
+
+	public static String encode(int num) {
+		StringBuilder str = new StringBuilder();
+		while (num > 0) {
+			str.insert(0, ALPHABET.charAt(num % BASE));
+			num = num / BASE;
+		}
+		return str.toString();
+	}
+
+	public static int decode(String str) {
+		int num = 0;
+		for (int i = 0; i < str.length(); i++) {
+			num = num * BASE + ALPHABET.indexOf(str.charAt(i));
+		}
+		return num;
+	}
+	public static void main(String[] args) {
+        int num = 122312215;
+        String url = encode(num);
+        System.out.println("short url:  " + url);
+        System.out.println("Id: " + decode(url));
+    }
+
+}
